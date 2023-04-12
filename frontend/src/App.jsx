f1bb282fcc61004856384ba5c663d680938ccf26
mlAgostini <valmont.vakarian@gmail.com>
2023-04-12 16:04:43 +0200

    component cart and cart item

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
 import "./App.css";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div>
+      <Cart />
     </div>
   );
 }
